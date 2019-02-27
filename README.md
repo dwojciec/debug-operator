24f7c57ca5d064368efb6e00466f83f81472858d
Didier Wojciechowski <dwojciec@redhat.com>
2019-02-27 17:07:16 +0100

    add squash-subscription.yaml

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # debug-operator
+
+'''
+$ oc new-project dw-squash
+$ oc create -f squash.v0.3.1.clusterserviceversion.yaml
+$ oc create -f squash-subscription.yaml
+$ oc adm policy add-cluster-role-to-user cluster-admin system:serviceaccount:dw-squash:squash-operator
+$ oc adm policy add-scc-to-user privileged -n dw-squash -z default
+'''
---
+++ install.sh
@@ -0,0 +1,7 @@
+oc new-project $1
+sed 's|REPLACE_NAMESPACE|$1|g' squash.v0.3.1.clusterserviceversion.yaml > squash.v0.3.1.clusterserviceversion-dev.yaml
+oc create -f squash.v0.3.1.clusterserviceversion-dev.yaml
+sed 's|REPLACE_NAMESPACE|$1|g' squash-subscription.yaml > squash-subscription-dev.yaml
+oc create -f squash-subscription-dev.yaml
+oc adm policy add-cluster-role-to-user cluster-admin system:serviceaccount:$1:squash-operator
+oc adm policy add-scc-to-user privileged -n $1 -z default
